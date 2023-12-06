fa219b21485cbfbbb938d012d0aa0d80a2b3b819
davi <daviglp12@gmail.com>
2023-12-06 07:25:28 -0300

    Ajuste de CORS para comunicação back-front end

--- src/main/java/com/dgomes/financas/FinancasApplication.java
+++ src/main/java/com/dgomes/financas/FinancasApplication.java
@@ -2,9 +2,20 @@ package com.dgomes.financas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class FinancasApplication {
+@EnableWebMvc
+public class FinancasApplication implements WebMvcConfigurer {
+
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		WebMvcConfigurer.super.addCorsMappings(registry);
+		registry.addMapping("/**").allowedMethods("GET", "POST", "DELETE", "PUT", "OPTIONS" );
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FinancasApplication.class, args);
